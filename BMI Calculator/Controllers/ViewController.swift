92e231ec98f5fcc002de1562381b5da464783940
Ordaz Romero Sergio <orcheko@gmail.com>
2022-09-01 02:07:06 -0500

    Definición de la clase SecondViewClass y obtención del valor de cada slider

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var heightValue: UILabel!
+    @IBOutlet var weightValue: UILabel!
+    @IBOutlet var heightSlider: UISlider!
+    @IBOutlet var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightValue.text = "\(height)m"
+    }
+    
+    
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        let weight = Int(sender.value)
+        weightValue.text = "\(weight)Kg"
+    }
+    
+    @IBAction func calculateBMI(_ sender: Any) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / pow(height, 2)
+    }
+    
 }
 
---
+++ BMI Calculator/Views/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Sergio Ordaz Romero on 01/09/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
